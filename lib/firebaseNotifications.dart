2d324658da838ca0a05ad41d7b5a4b3dbdc06f84
Ricardo Ogliari <rogliariping@gmail.com>
2021-03-02 19:53:17 -0300

    mian.dart pronta (sem gerencia de estados)

--- lib/firebaseNotifications.dart
+++ lib/firebaseNotifications.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_app_estados_push/model/payment.dart';
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_estados_push/firebaseNotifications.dart';
+import 'package:flutter_app_estados_push/model/payment.dart';
 
 void main() {
   runApp(MyApp());
@@ -46,68 +48,84 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
+  List<Payment> payments = [];
+
+  @override
+  void initState() {
+    super.initState();
+    new FirebaseNotifications(useMessage).setUpFirebase();
+  }
+
+  void useMessage(Payment payment){
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      payments.add(payment);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Contas a pagar"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: payments.length == 0 ? Container(
+          padding: EdgeInsets.all(20),
+          child: Text(
+            "Nenhuma conta recebida neste momento...",
+            style: TextStyle(
+                fontSize: 25
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          )
+      ): DataTable(
+        columns: <DataColumn>[
+          DataColumn(
+            label: Text(
+              'Tipo',
+              style: TextStyle(fontStyle: FontStyle.italic),
             ),
-          ],
-        ),
+          ),
+          DataColumn(
+            label: Text(
+              'Local',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+          DataColumn(
+            label: Text(
+              'Valor',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ],
+        rows: getRows(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  List<DataRow> getRows(){
+    List<DataRow> rows = [];
+
+    payments.forEach((element) {
+      rows.add(
+          DataRow(
+            selected: element.selected,
+            cells: <DataCell>[
+              DataCell(Text(element.type)),
+              DataCell(Text(element.place)),
+              DataCell(Text("${element.value}")),
+            ],
+            onSelectChanged: (value) {
+              setState(() {
+                element.selected = value;
+              });
+            },
+          )
+      );
+    });
+    return rows;
+  }
+
+
+
 }
